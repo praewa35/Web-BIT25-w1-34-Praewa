42288bd8d1382a5ad5042a5c4c59c18755b8b87b
USER922-34 <user@USER922-34.local>
2026-06-05 10:56:06 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>งานที่1 แพรวา พัฒนพูนผล BIT.2/5 เลขที่34</h1>
+
+    <form action="">
+        <label for=""></label>
+    </form>
+</body>
+</html>
